9d1b027c2d2fe3b72037ea90bd4d3661a5585c80
Guilherme Neves <guilhermemferraz@hotmail.com>
2025-04-04 17:32:02 -0300

    Avanti Seletiva

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function setupDropdown(buttonId, dropdownId) {
+    const button = document.getElementById(buttonId);
+    const dropdown = document.getElementById(dropdownId);
+
+    button.addEventListener("mouseenter", function () {
+      dropdown.classList.remove("hidden");
+    });
+
+    dropdown.addEventListener("mouseleave", function () {
+      dropdown.classList.add("hidden");
+    });
+
+    button.addEventListener("mouseleave", function (event) {
+      // Se o mouse não entrou no dropdown, escondemos o menu
+      setTimeout(() => {
+        if (!dropdown.matches(":hover")) {
+          dropdown.classList.add("hidden");
+        }
+      }, 100);
+    });
+  }
+
+  setupDropdown("departamentoBtn", "dropdown");
+  setupDropdown("departamentoBtn2", "dropdown2");
+});
+
